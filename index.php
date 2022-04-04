32273e91d1ee7b58d77118eab9fa74c5273d3b23
alleem18 <aleem2747@gmail.com>
2022-04-05 01:36:39 +0530

    career

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<htm lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Dashboard</title>
+</head>
+<b>
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+<!-- The sidebar -->
+<div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+<!-- Page content -->
+<div class="content">
+  <br><br>
+  <p>
+  
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    POST
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+  <form>
+  <div class="mb-3">
+    <label for="Company Name" class="form-label">Company Name</label>
+    <input type="text" class="form-control" id="company name" name="Cname">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPosition" class="form-label">Position</label>
+    <input type="text" class="form-control" id="exampleInputPosition" name="position">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputJobdesc" class="form-label">Job Description</label>
+    <input type="text" class="form-control" id="Jobdesc" name="Job Desc">
+  </div>
+  <div class="mb-3">
+    <label for="CTC" class="form-label">CTC</label>
+    <input type="text" class="form-control" id="CTC" name="CTC">
+  </div>
+  
+  <button type="submit" class="btn btn-primary" name = "job-submit">Submit</button>
+</form>
+  </div>
+</div>
+<table class="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">S.NO</th>
+      <th scope="col">Company Name</th>
+      <th scope="col">Position</th>
+      <th scope="col">CTC</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
